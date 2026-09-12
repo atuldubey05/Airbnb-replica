28ba4403786c0ed1f91f8d45779e384eedae83cd
atul <mr.atul0518@gmail.com>
2026-09-12 21:48:21 +0530

    add user model and add authentication

--- routes/review.js
+++ routes/review.js
@@ -22,6 +22,11 @@ router.post(
   validateReview,
   wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      req.flash("error", "Listing you are trying to review does not exist!");
+      return res.redirect("/listings");
+    }
+
     let newReview = new Review(req.body.review);
 
     listing.reviews.push(newReview);
