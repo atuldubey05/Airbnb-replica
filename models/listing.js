0755e3980a8b438cd58d38a5b2d277d69fc0648e
atul <mr.atul0518@gmail.com>
2026-09-13 21:28:28 +0530

    Add image istead of url link with the help of cloudinary and multer .

--- models/listing.js
+++ models/listing.js
@@ -9,13 +9,8 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1516815231560-8f41ec531527?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhY2glMjBob3RlbHxlbnwwfHwwfHx8MA%3D%3D",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1516815231560-8f41ec531527?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhY2glMjBob3RlbHxlbnwwfHwwfHx8MA%3D%3D"
-        : v,
+    url: String,
+    filename: String,
   },
   price: Number,
   location: String,
