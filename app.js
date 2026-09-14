27f9a28514ccba408ba9006ffbd7454b21e83a04
atul <mr.atul0518@gmail.com>
2026-09-14 15:26:45 +0530

    Add filters

--- app.js
+++ app.js
@@ -51,9 +51,9 @@ const sessionOptions = {
   },
 };
 
-app.get("/", (req, res) => {
-  res.send("Hii , i am root! ");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hii , i am root! ");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
